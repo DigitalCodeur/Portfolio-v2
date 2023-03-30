2ded0a6933e267b515edd53d85291d226eed39a9
LOUMON Koffi Samuel <lkoffisamuel@gmail.com>
2023-03-30 12:30:01 +0000

    add new portfolio

---
+++ js/app.js
@@ -0,0 +1,96 @@
+//============== scroll spy code start =================
+
+const scrollSpyOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: .4
+}
+
+/**
+ * @param {HTMLElement} elem
+ */
+
+const activate = function (elem) {
+    const id = elem.getAttribute('id')
+    const anchor = document.querySelector(`a[href="#${id}"]`)
+
+    if (anchor === null) {
+        return null
+    }
+
+    anchor.parentElement
+        .querySelectorAll('.active')
+        .forEach(function (node) {
+            node.classList.remove('active')
+        })
+
+    anchor.classList.add('active')
+}
+
+
+/**
+ * @param {IntersectionObserverEntry[]} scrollSpyEntries
+ * @param {IntersectionObserver} scrollSpyObserver
+ */
+
+const callback = function (scrollSpyEntries, scrollSpyObserver) {
+    scrollSpyEntries.forEach(function (scrollSpyEntry) {
+        if (scrollSpyEntry.intersectionRatio > scrollSpyOptions.threshold) {
+            activate(scrollSpyEntry.target)
+        }
+    })
+}
+
+const spies = document.querySelectorAll('[data-spy]')
+
+if (spies.length > 0) {
+    const scrollSpyObserver = new IntersectionObserver(callback, scrollSpyOptions)
+
+    spies.forEach(function (spy) {
+        scrollSpyObserver.observe(spy)
+    })
+}
+
+// ================= scroll spy code end ============
+
+// ============= Ajax Form start ===============
+
+const myForm = document.getElementById("myform");
+
+myForm.addEventListener("submit", function (e) {
+    e.preventDefault();// Prevent page from reloading after submission
+
+    const xhttp = new XMLHttpRequest();
+    const data = new FormData(myForm);// Retrieve form data
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const object = document.getElementById("object").value;
+    const message = document.getElementById("message").value;
+
+    // We check if all the fields are filled
+
+    if (name && email && object && message) {
+        xhttp.onload = function () {
+            const succesModal = document.querySelector(".success");
+            succesModal.classList.add('success-show');
+
+            setTimeout(() => {
+                succesModal.classList.remove('success-show');
+            }, 2000);
+        }
+
+        xhttp.open("POST", "https://scodeur.alwaysdata.net/php/form.php"); // send to php without reloading the page
+
+        console.log(xhttp)
+
+        xhttp.send(data);
+        myForm.reset();
+    } else {
+        alert("Veillez remplir tous les champs");
+    }
+
+});
+
+
+// ============= Ajax Form end ===============
